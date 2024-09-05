cb802a61f2a02295055c3ac61f9c6cfc637eb27a
leonardo.morita <leonardo.morita@bistwobis.com.br>
2024-09-04 21:51:39 -0300

    ✨ Aula 03

---
+++ js/dados.js
@@ -0,0 +1,32 @@
+let lionelMessi = {
+    nome: 'Lionel Messi',
+    descricao: 'Lionel Messi, considerado por muitos o maior jogador de futebol de todos os tempos, é um craque argentino que encanta o mundo com sua habilidade única e seus gols espetaculares.',
+    links: {
+        instagram: 'https://www.instagram.com/leomessi/',
+        wiki: 'https://pt.wikipedia.org/wiki/Lionel_Messi'
+    }
+};
+
+let ronaldinhoGaucho = {
+    nome: 'Ronaldinho Gaúcho',
+    descricao: 'Ronaldinho Gaúcho, um dos maiores gênios do futebol, encantou o mundo com seu futebol alegre e habilidoso, sendo um dos principais responsáveis pela conquista da Liga dos Campeões pelo Barcelona em 2006.',
+    links: {
+        instagram: 'https://www.instagram.com/ronaldinho/',
+        wiki: 'https://pt.wikipedia.org/wiki/Ronaldinho_Gaúcho'
+    }
+};
+
+let johanCruyff = {
+    nome: 'Johan Cruyff',
+    descricao: 'Johan Cruyff, além de ser um dos maiores jogadores da história, revolucionou o futebol com seu estilo de jogo e sua filosofia, sendo considerado um dos pais do "futebol total".',
+    links: {
+        instagram: 'https://www.instagram.com/johancruyff/',
+        wiki: 'https://pt.wikipedia.org/wiki/Johan_Cruyff'
+    }
+};
+
+let dados = [
+    lionelMessi,
+    ronaldinhoGaucho,
+    johanCruyff
+];
